ddf73c50e58e6c241e9d0379202feca8b6020152
Din Sebastian <dinsebi22@gmail.com>
2021-05-05 12:18:23 +0300

    Add files via upload

---
+++ meal-service-v2/src/components/Cart/Cart.js
@@ -0,0 +1,52 @@
+import React, { useContext } from "react";
+import CartContext from "../../store/cart-context";
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+
+const Cart = (props) => {
+  const ctx = useContext(CartContext);
+  const totalAmount = `$${ctx.totalAmount.toFixed(2)}`;
+
+  const hasItems = ctx.items.length > 0;
+
+  const cartItemToBeRemovedHadler = (id) => {
+    ctx.removeItem(id);
+  };
+  const cartItemToBeAddHadler = (item) => {
+    ctx.addItem(item);
+  };
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {ctx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemToBeRemovedHadler.bind(null, item.id)}
+          onAdd={cartItemToBeAddHadler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={classes.actions}>
+        <button onClick={props.onClose} className={classes["button--alt"]}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Checkout</button>}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
